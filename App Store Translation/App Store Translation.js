457ad431e6deef16c5d9d904f8c0b2f0c78bf305
侯猛 <houmeng@outlook.it>
2019-08-21 14:47:53 +0800

    翻译增加显示原文

--- App Store Translation/App Store Translation.js
+++ App Store Translation/App Store Translation.js
@@ -1,18 +1,15 @@
-/*
-version-1.5-version
-updateContent-去除脚本自动检查更新，以后的更新依赖于 Erots 脚本商店，本次更新之后打开脚本会提示安装 Erots 脚本商店。-updateContent
-installUrl-jsbox://import?name=App Store Translation&url=https://raw.githubusercontent.com/nlnlnull/xTeko/master/App%20Store%20Translation/App%20Store%20Translation.js&icon=icon_162.png-installUrl
-*/
-/**erots
-id: 5cea8d75d5de2b0070730938
-build: 5
-*/
-
-// var tkkNumber = ""
 // 更新内容 软件简介
 var updateContent = "", descriptionContent = ""
+// 更新内容 软件简介 英文原文
+var updateContentOriginal = "", descriptionContentOriginal = ""
 // 更新内容翻译集合 软件简介翻译集合
 var updateContentGroup = [], descriptionContentGroup = []
+
+var introduction = ""
+
+var title = ""
+
+var currentDisplayContent = 1
 
 // 翻译步骤标识 
 // 1 正在翻译更新内容 
@@ -48,77 +45,6 @@ if (($app.env == $env.action) && link) {
   if (appid) {
     lookup(appid, region)
   }
-} else if ($app.env == $env.app) {
-  if (erotsInstall() == false && erotsInstall2() == false) {
-    $ui.alert({
-      title: "提示",
-      message: "您尚未安装 Erots 脚本商店，此脚本已取消自动更新机制，以后的更新都在 Erots 脚本商店中发布，是否安装 Erots 脚本商店？（点击忽略则以后不再提示）",
-      actions: [{
-        title: "忽略",
-        handler: function () {
-          let data = $addin.current.data.string
-          let newData = "//install" + "Erots\n" + data
-          var newDatata = $data({
-            string: newData
-          })
-          $addin.save({
-            name: "App Store Translation",
-            data: newDatata
-          });
-        }
-      },
-      {
-        title: "安装",
-        handler: function () {
-          $ui.loading(true);
-          $http.download({
-            url: "https://github.com/LiuGuoGY/JSBox-addins/raw/master/Erots/.output/Erots.box?raw=true",
-            showsProgress: false,
-            timeout: 5,
-            progress: function (bytesWritten, totalBytes) {
-              var percentage = bytesWritten * 1.0 / totalBytes
-              $ui.progress(percentage, "下载中...")
-            },
-            handler: function (resp) {
-              var file = resp.data;
-              $addin.save({
-                name: "Erots",
-                data: file,
-                handler: function (success) {
-                  $ui.loading(false);
-                  $ui.alert({
-                    title: "安装完成",
-                    actions: [{
-                      title: "确定",
-                      handler: function () {
-                        if ($app.env = $env.app) {
-                          $addin.run("Erots")
-                        }
-                      }
-                    }],
-                  });
-                }
-              });
-            }
-          });
-        }
-      }
-      ]
-    });
-  } else {
-    $ui.alert({
-      title: "环境错误",
-      message: "请在 App Store 应用详情页分享运行此脚本",
-      actions: [
-        {
-          title: "我知道了",
-          disabled: false, // Optional
-          handler: function () {
-          }
-        }
-      ]
-    })
-  }
 } else {
   $ui.alert({
     title: "环境错误",
@@ -135,21 +61,25 @@ if (($app.env == $env.action) && link) {
 }
 
 function lookup(appid, region) {
+  let url = "https://itunes.apple.com/" + region + "/lookup?id=" + appid
+  $console.info(url)
   $http.get({
-    url: "https://itunes.apple.com/" + region + "/lookup?id=" + appid,
+    url: url,
     handler: function (resp) {
       $ui.loading(false)
       if (resp.data) {
         $delay(0.5, function () {
           var result = resp.data.results[0]
-          var description = result.description
-          var releaseNotes = result.releaseNotes
-          googleTran(releaseNotes, description)
+          updateContentOriginal = result.releaseNotes
+          descriptionContentOriginal = result.description
+          title = result.trackName
+          introduction = "AppID：" + result.trackId + "\n当前版本：" + result.version + "\n最近更新日期：" + result.currentVersionReleaseDate.replace(/[a-z,A-Z]/g, " ") + "\n当前价格：" + result.currency + "->" + result.formattedPrice + "\nbundleId：" + result.bundleId
+          googleTran(updateContentOriginal)
         })
       } else {
         $ui.alert({
-          title: "网络错误",
-          message: "请检查网络，并重新尝试翻译，若还存在问题，请尝试打开科学上网，脚本依赖于 Google 翻译。",
+          title: "获取应用详情失败",
+          message: "请检查网络，并重新尝试运行脚本，若还是存在问题，请尝试打开科学上网。",
           actions: [
             {
               title: "我知道了",
@@ -165,7 +95,7 @@ function lookup(appid, region) {
 }
 
 //谷歌翻译
-function googleTran(text, descriptionC) {
+function googleTran(text) {
   if (translationStep == 1) {
     $ui.loading("翻译中...")
   }
@@ -193,7 +123,7 @@ function googleTran(text, descriptionC) {
           // 更新内容翻译完毕
           updateContentGroup = resp.data.sentences
           translationStep = 2
-          googleTran(descriptionC, "")
+          googleTran(descriptionContentOriginal, "")
         } else if (translationStep == 2) {
           $ui.loading(false)
           // 软件简介翻译完毕
@@ -227,7 +157,6 @@ function googleTran(text, descriptionC) {
 
 
 function show(updateSentences, descriptionSentences) {
-  checkupVersion()
   var updateText = "", descriptionText = ""
   var updateLength = updateSentences.length
   var descriptionLength = descriptionSentences.length
@@ -243,6 +172,26 @@ function show(updateSentences, descriptionSentences) {
   $console.info(descriptionText)
 
   $ui.render({
+    props: {
+      title: title,
+      navButtons: [
+        {
+          title: "Title",
+          icon: "162", // Or you can use icon name
+          handler: function () {
+            if (currentDisplayContent == 1) {
+              $("update-content").text = updateContentOriginal
+              $("introduction-content").text = descriptionContentOriginal
+              currentDisplayContent = 2
+            } else {
+              $("update-content").text = updateText
+              $("introduction-content").text = descriptionText
+              currentDisplayContent = 1
+            }
+          }
+        }
+      ]
+    },
     views: [{
       type: "scroll",
       layout: $layout.fill,
@@ -266,6 +215,33 @@ function show(updateSentences, descriptionSentences) {
       views: [{
         type: "label",
         props: {
+          text: "当前版本：",
+          lines: 0,
+          font: updateTitleFont,
+          textColor: updateTitleColor,
+          id: "update-version-title"
+        },
+        layout: function (make, view) {
+          make.top.left.inset(10)
+          make.width.equalTo(view.super).offset(-20)
+        }
+      }, {
+        type: "label",
+        props: {
+          text: introduction,
+          lines: 0,
+          font: introductionContentFont,
+          textColor: introductionContentColor,
+          id: "update-version"
+        },
+        layout: function (make, view) {
+          make.top.equalTo($("update-version-title").bottom).offset(5)
+          make.left.inset(10)
+          make.width.equalTo(view.super).offset(-20)
+        }
+      }, {
+        type: "label",
+        props: {
           text: "更新内容：",
           lines: 0,
           font: updateTitleFont,
@@ -273,7 +249,8 @@ function show(updateSentences, descriptionSentences) {
           id: "update-title"
         },
         layout: function (make, view) {
-          make.top.left.inset(10)
+          make.top.equalTo($("update-version").bottom).offset(30)
+          make.left.inset(10)
           make.width.equalTo(view.super).offset(-20)
         }
       },
@@ -302,7 +279,7 @@ function show(updateSentences, descriptionSentences) {
           id: "introduction-title"
         },
         layout: function (make, view) {
-          make.top.equalTo($("update-content").bottom).offset(50)
+          make.top.equalTo($("update-content").bottom).offset(30)
           make.left.equalTo($("update-content"))
           make.width.equalTo($("update-content"))
         }
@@ -325,137 +302,4 @@ function show(updateSentences, descriptionSentences) {
       }]
     }]
   })
-}
-
-//检查版本
-function checkupVersion() {
-  return
-  $ui.loading("正在检查更新...");
-  $http.download({
-    url: "https://raw.githubusercontent.com/nlnlnull/xTeko/master/App%20Store%20Translation/App%20Store%20Translation.js",
-    showsProgress: false,
-    timeout: 5,
-    handler: function (resp) {
-      $ui.loading(false);
-      let newData = resp.data.string
-      $console.info(newData);
-      let versionRegex = /version-([\s\S]*?)-version/
-      let updateContentRegex = /updateContent-([\s\S]*?)-updateContent/
-      let installUrlRegex = /installUrl-([\s\S]*?)-installUrl/
-      let newVersion = versionRegex.exec(newData)[1]
-      let newUpdateContent = updateContentRegex.exec(newData)[1]
-      let newInstallUrl = installUrlRegex.exec(newData)[1]
-      $console.info(newVersion)
-      $console.info(newUpdateContent)
-      $console.info(newInstallUrl)
-      let oldData = $addin.current.data.string
-      let oldVersion = versionRegex.exec(oldData)[1]
-      if (newVersion > oldVersion && $app.env == $env.app) { // 有新版本 并且是在主程序运行
-        $ui.alert({
-          title: "发现新版本",
-          message: newUpdateContent,
-          actions: [
-            {
-              title: "取消",
-              disabled: false, // Optional
-              handler: function () {
-
-              }
-            },
-            {
-              title: "更新",
-              disabled: false, // Optional
-              handler: function () {
-                updateAddin(resp.data)
-                // $app.openURL(encodeURI(newInstallUrl))
-                // $app.close()
-              }
-            }
-          ]
-        })
-      } else if (newVersion > oldVersion && $app.env != $env.app) {  // 有新版本 但是在非主程序运行
-        $ui.alert({
-          title: "发现新版本",
-          message: "请在主程序运行本脚本更新",
-          actions: [
-            {
-              title: "我知道了",
-              disabled: false, // Optional
-              handler: function () {
-              }
-            }
-          ]
-        })
-      } else if (newVersion <= oldVersion && $app.env == $env.app) {  // 没有新版本 但是在主程序运行
-        $ui.alert({
-          title: "没有发现新版本",
-          message: "请在 App Store 应用详情页分享运行此脚本",
-          actions: [
-            {
-              title: "我知道了",
-              disabled: false, // Optional
-              handler: function () {
-              }
-            }
-          ]
-        })
-      }
-    }
-  })
-}
-//当前插件名
-function currentName() {
-  let name = $addin.current.name
-  let end = name.length - 3
-  return name.substring(0, end)
-}
-
-//升级插件
-function updateAddin(app) {
-  $addin.save({
-    name: currentName(),
-    data: app,
-    icon: $addin.current.icon,
-    handler: function (success) {
-      if (success) {
-        $device.taptic(2)
-        $delay(0.15, function () {
-          $device.taptic(2)
-        })
-        $ui.alert({
-          title: "升级完成",
-          actions: [{
-            title: "OK",
-            handler: function () {
-              $app.openExtension($addin.current.name)
-            }
-          }
-          ]
-        })
-      }
-    }
-  })
-}
-
-
-
-function erotsInstall() {
-  var addins = $addin.list
-  let i = addins.length
-  while (i--) {
-    let item = addins[i]
-    let name = item.name
-    if (name == "Erots") {
-      return true
-    }
-  }
-  return false
-}
-function erotsInstall2() {
-  var dataString = $addin.current.data.string
-  if (dataString.indexOf("install" + "Erots") != -1) {
-    return true
-  } else {
-    return false
-  }
 }
