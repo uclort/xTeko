c6742f0eb3eb54fd477c2e2c151c52f2196b41a8
nlnlnull <houmeng@outlook.it>
2019-10-14 14:08:14 +0800

    更新

--- 12306/scripts/home.js
+++ 12306/scripts/home.js
@@ -6,13 +6,8 @@ var timetable = require('scripts/timetable')
 var ticketGate = require('scripts/ticketGate')
 // 正晚点查询
 var punctuality = require('scripts/punctuality')
-
-currentVersion = $addin.current.version
-
-if (currentVersion == undefined) {
-    $addin.current.version = "1.0.0"
-    currentVersion = $addin.current.version
-}
+// 正晚点查询
+var update = require('scripts/update')
 
 module.exports.render = function render() {
     $ui.render({
@@ -91,22 +86,13 @@ module.exports.render = function render() {
     })
 }
 
-checkupVersion()
-
 //检查版本
-function checkupVersion() {
+function updateData() {
     $http.get({
         url: "https://raw.githubusercontent.com/nlnlnull/xTeko/master/12306/UpdateInfo",
         handler: function (resp) {
-            $console.info(resp.data)
-            let version = resp.data.version
-            let message = resp.data.message
-            let updateUrl = resp.data.updateUrl
-            let updateFileUrl = resp.data.updateFileUrl
-
             let excessTicketInquiryUrl = resp.data.excessTicketInquiryUrl
             let cookie = resp.data.Cookie
-
             let excessTicketInquiryUrl_Old = $cache.get("excessTicketInquiryUrl")
             let cookie_Old = $cache.get("cookie")
             if (!excessTicketInquiryUrl_Old || excessTicketInquiryUrl != excessTicketInquiryUrl_Old) {
@@ -117,78 +103,9 @@ function checkupVersion() {
                 $cache.set("cookie", cookie)
                 // $ui.toast("Cookie 已更新");
             }
-            $console.info(version);
-            $console.info(currentVersion);
-
-            if (versionCmp(version, $addin.current.version) == 1) {
-                $ui.alert({
-                    title: "发现新版本",
-                    message: message,
-                    actions: [
-                        {
-                            title: "更新",
-                            handler: function () {
-                                $http.download({
-                                    url: updateFileUrl,
-                                    progress: function (bytesWritten, totalBytes) {
-                                        var percentage = bytesWritten * 1.0 / totalBytes
-                                    },
-                                    handler: function (resp) {
-                                        var file = resp.data;
-                                        $addin.save({
-                                            name: $addin.current.name,
-                                            icon: $addin.current.icon,
-                                            data: file,
-                                            handler: function (success) {
-                                                if (success) {
-                                                    $addin.current.version = version
-                                                    $addin.restart()
-                                                }
-                                            }
-                                        })
-                                    }
-                                });
-                            }
-                        }
-                    ]
-                });
-            }
-
         }
     })
 }
 
-// 不考虑字母
-function s2i(s) {
-    return s.split('').reduce(function (a, c) {
-        var code = c.charCodeAt(0);
-        if (48 <= code && code < 58) {
-            a.push(code - 48);
-        }
-        return a;
-    }, []).reduce(function (a, c) {
-        return 10 * a + c;
-    }, 0);
-}
-
-function versionCmp(s1, s2) {
-    var a = s1.split('.').map(function (s) {
-        return s2i(s);
-    });
-    var b = s2.split('.').map(function (s) {
-        return s2i(s);
-    });
-    var n = a.length < b.length ? a.length : b.length;
-    for (var i = 0; i < n; i++) {
-        if (a[i] < b[i]) {
-            return -1;
-        } else if (a[i] > b[i]) {
-            return 1;
-        }
-    }
-    if (a.length < b.length) return -1;
-    if (a.length > b.length) return 1;
-    var last1 = s1.charCodeAt(s1.length - 1) | 0x20,
-        last2 = s2.charCodeAt(s2.length - 1) | 0x20;
-    return last1 > last2 ? 1 : last1 < last2 ? -1 : 0;
-}
+updateData()
+update.checkupVersion("https://raw.githubusercontent.com/nlnlnull/xTeko/master/12306/UpdateInfo")
---
+++ 12306/scripts/update.js
@@ -0,0 +1,96 @@
+currentVersion = $addin.current.version
+
+if (currentVersion == undefined) {
+  $addin.current.version = "1.0.0"
+  currentVersion = $addin.current.version
+}
+
+module.exports = {
+  checkupVersion: checkupVersion
+}
+
+//检查版本
+function checkupVersion(url) {
+  $http.get({
+    url: url,
+    handler: function (resp) {
+      $console.info(resp.data)
+      let version = resp.data.version
+      let message = resp.data.message
+      let updateFileUrl = resp.data.updateFileUrl
+
+      // $console.info(version);
+      // $console.info(currentVersion);
+
+      if (versionCmp(version, currentVersion) == 1) {
+        $ui.alert({
+          title: "发现新版本",
+          message: message,
+          actions: [
+            {
+              title: "更新",
+              handler: function () {
+                $http.download({
+                  url: updateFileUrl,
+                  progress: function (bytesWritten, totalBytes) {
+                    var percentage = bytesWritten * 1.0 / totalBytes
+                    $ui.progress(percentage)
+                  },
+                  handler: function (resp) {
+                    var file = resp.data;
+                    $addin.save({
+                      name: $addin.current.name,
+                      icon: $addin.current.icon,
+                      data: file,
+                      handler: function (success) {
+                        if (success) {
+                          $addin.current.version = version
+                          $addin.restart()
+                        }
+                      }
+                    })
+                  }
+                });
+              }
+            }
+          ]
+        });
+      }
+    }
+  })
+}
+
+function versionCmp(s1, s2) {
+  var a = s1.split('.').map(function (s) {
+    return s2i(s);
+  });
+  var b = s2.split('.').map(function (s) {
+    return s2i(s);
+  });
+  var n = a.length < b.length ? a.length : b.length;
+  for (var i = 0; i < n; i++) {
+    if (a[i] < b[i]) {
+      return -1;
+    } else if (a[i] > b[i]) {
+      return 1;
+    }
+  }
+  if (a.length < b.length) return -1;
+  if (a.length > b.length) return 1;
+  var last1 = s1.charCodeAt(s1.length - 1) | 0x20,
+    last2 = s2.charCodeAt(s2.length - 1) | 0x20;
+  return last1 > last2 ? 1 : last1 < last2 ? -1 : 0;
+}
+
+// 不考虑字母
+function s2i(s) {
+  return s.split('').reduce(function (a, c) {
+    var code = c.charCodeAt(0);
+    if (48 <= code && code < 58) {
+      a.push(code - 48);
+    }
+    return a;
+  }, []).reduce(function (a, c) {
+    return 10 * a + c;
+  }, 0);
+}
