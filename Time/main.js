1413b6ebc97e6bda37e96d93b13786ceba4dffba
nlnlnull <houmeng@outlook.it>
2019-11-19 17:32:37 +0800

    首发版本

--- Time/main.js
+++ Time/main.js
@@ -1,3 +1,5 @@
 let time = require('scripts/timeList')
 let update = require('scripts/update')
 time.render()
+
+update.checkupVersion("https://raw.githubusercontent.com/nlnlnull/xTeko/master/Time/UpdateInfo")
